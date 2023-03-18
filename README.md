e66449b0c3b98b175d059c74320fcc5a24ce65ea
fula04 <65560849+fula04@users.noreply.github.com>
2023-03-18 16:46:49 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Ejercicio-24-A2
+# Ejercicio-24-A2
---
+++ src/main/java/com/diegofula/ejercicio24/ejercicio24.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.diegofula.ejercicio24;
+
+/**
+ *
+ * @author RyZen 5 Pro
+ */
+public class ejercicio24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
